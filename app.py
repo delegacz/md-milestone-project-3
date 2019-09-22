c90a7ad6af7428670aa2661a5c0c72c08c2b2d48
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-09-22 13:30:54 +0100

    enviorment virables set up, database connection  established

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('Hell0')
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+#enviorement virables loading
+load_dotenv()
+DBNAME = os.getenv("DBNAME")
+DATABASEURI = os.getenv("URI")
+
+app = Flask(__name__)
+
+#Connecting to database
+app.config["MONGO_DBNANE"] = DBNAME
+app.config["MONGO_URI"] = DATABASEURI
+mongo = PyMongo(app)
