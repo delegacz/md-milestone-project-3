0aaa70ae8a8ea9934c6c14e03c7778e666cdff1b
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-09-22 17:25:04 +0100

    database connection established and tested, template files created and tested

--- app.py
+++ app.py
@@ -14,4 +14,17 @@ app = Flask(__name__)
 #Connecting to database
 app.config["MONGO_DBNANE"] = DBNAME
 app.config["MONGO_URI"] = DATABASEURI
+
 mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/home')
+def display():
+    return render_template("home.html", 
+                           playlists=mongo.db.playslist.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=(os.environ.get('PORT')),
+    debug=True)
