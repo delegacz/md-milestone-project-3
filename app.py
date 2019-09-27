cb0572a75f0269ff0491878351060ff541729264
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-09-27 23:00:41 +0100

    added editing and update functionality

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+@app.route('/home')
 def display():
     return render_template("home.html", 
                            playlists=mongo.db.playlists.find())
@@ -30,12 +31,33 @@ def add():
                             categories=mongo.db.categories.find())
 
 @app.route('/insert', methods=['POST','GET'])
-def inert_playlist():
+def insert_playlist():
     playlists=mongo.db.playlists
     playlists.insert_one(request.form.to_dict())
     return redirect(url_for('display'))
 
 
+@app.route('/edit/<playlist_id>')
+def edit_playlist(playlist_id):
+    the_playlist = mongo.db.playlists.find_one({'_id': ObjectId(playlist_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit.html', playlist=the_playlist,
+                                        categories=all_categories)
+
+@app.route('/update_playlist/<playlist_id>',methods=['POST'])
+def update_playlist(playlist_id):
+    playlist = mongo.db.playlists
+    playlist.update(
+        {'_id': ObjectId(playlist_id)},
+        {
+            'name':request.form.get('name'),
+            'category':request.form.get('category_name'),
+            'description': request.form.get('description'),
+            'spotify_id':request.form.get('spotify_id'),
+            'spotify_link':request.form.get('spotify_link'),
+        }
+    )
+    return redirect(url_for('display'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
