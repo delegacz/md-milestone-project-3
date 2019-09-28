a90f8cea1afd34f7ade26573986017201e522c97
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-09-28 20:00:50 +0100

    setting up secret virables for heroku 3

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ load_dotenv()
 DBNAME = os.getenv("DBNAME")
 DATABASEURI = os.getenv("URI")
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
+app = Flask(__name__)
 
 #Connecting to database
 app.config["MONGO_DBNANE"] = DBNAME
